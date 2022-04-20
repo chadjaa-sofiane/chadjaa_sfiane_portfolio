ce518d75c4f3cff2500478d2717235352f11d241
Chadjaa Sofianne <67962574+chadjaa-sofiane@users.noreply.github.com>
2022-04-20 13:31:56 +0100

    add animation text

--- components/Footer/SocialMediaCard.tsx
+++ components/Footer/SocialMediaCard.tsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from "react"
+import { gsap } from "gsap"
 import styles from "./Footer.module.scss"
 
 interface Props {
@@ -7,12 +9,26 @@ interface Props {
 }
 
 const SocialMediaCard = ({ children, color = "white", url = "" }: Props) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const { current } = ref;
+        if (current) {
+            gsap.from(current, {
+                duration: 1,
+                scale: 0.95,
+                ease: "power3.out",
+                yoyo: true,
+                repeat: -1,
+            });
+        }
+    }, []);
     const classes = [
         styles[`icon--${color}`],
         styles["footer__socialMedia__card"]
     ]
     return (
-        <div className={classes.join(" ")}>
+        <div ref={ref} className={classes.join(" ")}>
             <a href={url} target="_blank" rel="noopener noreferrer">
                 {children}
             </a>
--- containers/Home/AboutMe/AboutMe.tsx
+++ containers/Home/AboutMe/AboutMe.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { LargeParagraph, Title1 } from "@components/core/Typography";
+import { LargeParagraph } from "@components/core/Typography";
+import { AnimationText } from "@components/AnimationText"
 import { Button } from "@components/core/Button";
 import Wrapper from "@components/core/Wrapper";
 import Container from "@components/core/Container";
@@ -11,7 +12,7 @@ const AboutMe = () => {
       <Container>
         <div className={styles["aboutMe__container"]}>
           <div className={styles["aboutMe__content"]}>
-            <Title1>Chadjaa Sofiane</Title1>
+            <AnimationText>Chadjaa Sofiane</AnimationText>
             <LargeParagraph>
               my name is chadjaa sofiane and I can build your dreams and hobs
               and bring you with me to the hell.
