b050f8e4ec8e3d154e6c45b4a7c91de9875db8d9
Chadjaa Sofianne <67962574+chadjaa-sofiane@users.noreply.github.com>
2022-04-30 02:34:59 +0100

    add the firebase configuration to the env

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "@firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0YzkxBJTtCNNVqXMQc7sRkPV2Yn5jQIw",
-  authDomain: "softfolio-cccda.firebaseapp.com",
-  projectId: "softfolio-cccda",
-  storageBucket: "softfolio-cccda.appspot.com",
-  messagingSenderId: "1054841022699",
-  appId: "1:1054841022699:web:54e3ae1dbcbad276be76ae",
-  measurementId: "G-BBZCTVXDWS",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STURAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
