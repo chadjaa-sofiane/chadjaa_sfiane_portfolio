813c000fa875785e4e5b2a24306e32db7e93dad0
Chadjaa Sofianne <67962574+chadjaa-sofiane@users.noreply.github.com>
2022-06-12 17:38:27 +0100

    remove about me button

--- containers/Home/HeroSection/HeroSection.tsx
+++ containers/Home/HeroSection/HeroSection.tsx
@@ -1,6 +1,5 @@
 import HeroIllustration from "./HeroIllustration";
 import { Section, SectionContent } from "@components/Section";
-import { Button } from "@components/core/Button";
 import { HightlightedText } from "@components/core/Typography";
 
 const HeroSection = () => {
@@ -9,7 +8,6 @@ const HeroSection = () => {
       <SectionContent
         title=" welcome in sotfolio"
         description={<>I&apos;m a <HightlightedText>full-stack</HightlightedText> developer with a passion for building beautiful, responsive websites and applications.</>}
-        button={<Button> about me </Button>}
       />
       <HeroIllustration />
     </Section>
