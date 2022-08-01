0d045347e770d802f46dfda105ee123384dffc46
chadjaa sofiane <sofianne012@gmail.com>
2022-08-01 16:51:06 +0100

    remove the incorrect firebase import

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "@firebase/storage";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
