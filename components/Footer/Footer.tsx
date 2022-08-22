5db673071ceb8e90e6c31065ef1fb3f394979503
chadjaa sofiane <sofianne012@gmail.com>
2022-08-22 18:43:40 +0100

    move the links from footer to the about section.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,3 @@
-import SocialMediaCard from "./SocialMediaCard";
-import TwitterIcon from "@svg/twitter.svg";
-import LinkedInIcon from "@svg/linked_in.svg";
-import GithubIcon from "@svg/github.svg";
 import styles from "./Footer.module.scss";
 import Container from "@components/core/Container";
 
@@ -11,11 +7,6 @@ const Footer = () => {
   return (
     <div className={styles["footer__wrapper"]}>
       <Container classNames={styles["footer__container"]}>
-        <div className={styles["footer__socialMedia__container"]}>
-          <TwitterIconWrapper />
-          <LinkedInIconWrapper />
-          <GitHubIconWrapper />
-        </div>
         <div className={styles["footer__text"]}>
           <p>© {year}</p>
         </div>
@@ -24,23 +15,5 @@ const Footer = () => {
   );
 };
 
-const TwitterIconWrapper = () => (
-  <SocialMediaCard url="https://twitter.com/ChadjaaSofiane" color="twitter">
-    <TwitterIcon />
-  </SocialMediaCard>
-);
-const LinkedInIconWrapper = () => (
-  <SocialMediaCard
-    url="https://www.linkedin.com/in/chadjaa-sofiane-749045217/"
-    color="linkedIn"
-  >
-    <LinkedInIcon />
-  </SocialMediaCard>
-);
-const GitHubIconWrapper = () => (
-  <SocialMediaCard url="https://www.github.com/chadjaa-sofiane" color="github">
-    <GithubIcon />
-  </SocialMediaCard>
-);
 
 export default Footer;
--- containers/Home/AboutMe/AboutMe.tsx
+++ containers/Home/AboutMe/AboutMe.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { LargeParagraph } from "@components/core/Typography";
-import { AnimationText } from "@components/AnimationText"
 import { Button } from "@components/core/Button";
 import Wrapper from "@components/core/Wrapper";
 import Container from "@components/core/Container";
+import TwitterIcon from "@svg/twitter.svg";
+import LinkedInIcon from "@svg/linked_in.svg";
+import GithubIcon from "@svg/github.svg";
 import styles from "./AboutMe.module.scss";
-import { HightlightedText } from "@components/core/Typography";
 
+import { HightlightedText } from "@components/core/Typography";
+import { AnimationText } from "@components/AnimationText";
+import { useSectionsProgress } from "@components/SectionsProgress";
 
 const downloadCv = () => {
   const a: HTMLAnchorElement = document.createElement("a");
@@ -18,17 +22,28 @@ const downloadCv = () => {
 };
 
 const AboutMe = ({ profileImage }: { profileImage: string }) => {
+  const { ref } = useSectionsProgress();
   return (
     <Wrapper>
       <Container>
-        <div className={styles["aboutMe__container"]}>
-          <div className={styles["aboutMe__content"]}>
-            <AnimationText>Chadjaa Sofiane</AnimationText>
-            <LargeParagraph>
-              my name is chadjaa sofiane. I&apos;am a <HightlightedText>full-stack</HightlightedText> developer.
-              <br />
-            </LargeParagraph>
-            <Button variant="outlined" onClick={downloadCv}> download my cv </Button>
+        <div ref={ref} className={styles["aboutMe__wrapper"]}>
+          <div className={styles["aboutMe"]}>
+            <div className={styles["aboutMe__links"]}>
+              <Twitter />
+              <LinkedIn />
+              <GitHub />
+            </div>
+            <div className={styles["aboutMe__content"]}>
+              <AnimationText> Chadjaa Sofiane </AnimationText>
+              <LargeParagraph>
+                my name is chadjaa sofiane. I&apos;am a
+                <HightlightedText> full-stack </HightlightedText> developer.
+                <br />
+              </LargeParagraph>
+              <Button variant="outlined" onClick={downloadCv}>
+                download my cv
+              </Button>
+            </div>
           </div>
           <div className={styles["aboutMe__image__field"]}>
             <Image src={profileImage || ""} alt="my photo" layout="fill" />
@@ -36,6 +51,45 @@ const AboutMe = ({ profileImage }: { profileImage: string }) => {
         </div>
       </Container>
     </Wrapper>
+  );
+};
+
+const Twitter = () => {
+  return (
+    <a
+      href="https://twitter.com/ChadjaaSofiane"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${styles["icon"]} ${styles["icon--twitter"]}`}
+    >
+      <TwitterIcon />
+    </a>
+  );
+};
+
+const LinkedIn = () => {
+  return (
+    <a
+      href="https://www.linkedin.com/in/chadjaa-sofiane-749045217/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${styles["icon"]} ${styles["icon--linkedin"]}`}
+    >
+      <LinkedInIcon />
+    </a>
+  );
+};
+
+const GitHub = () => {
+  return (
+    <a
+      href="https://www.github.com/chadjaa-sofiane"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${styles["icon"]} ${styles["icon--github"]}`}
+    >
+      <GithubIcon />
+    </a>
   );
 };
 
