786f5696e46b74d7766247fcb119976116e9544e
chadjaa-sofiane <sofianne012@gmail.com>
2024-03-16 02:48:16 +0100

    Implementing the mortal to the Modal.
    no more scrolling issues.

--- components/core/Modal/Modal.tsx
+++ components/core/Modal/Modal.tsx
@@ -1,43 +1,71 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 import styles from "./Modal.module.scss";
 
+const usePortal = () => {
+  const containerRef = useRef<HTMLDivElement>(document.createElement("div"));
+  useLayoutEffect(() => {
+    const container = containerRef.current;
+
+    if (container) {
+      container.id = "dialog-container";
+      container.style.zIndex = "9999";
+      document.body.appendChild(container);
+    }
+    return () => {
+      if (container) {
+        document.body.removeChild(container);
+      }
+    };
+  }, []);
+
+  return containerRef.current;
+};
+
 interface Props {
-    isOpen: boolean;
-    // eslint-disable-next-line no-unused-vars
-    setOpen: (isOpen: boolean) => void;
-    children: React.ReactNode;
+  isOpen: boolean;
+  // eslint-disable-next-line no-unused-vars
+  setOpen: (isOpen: boolean) => void;
+  children: React.ReactNode;
 }
 
 const Modal = ({ isOpen, setOpen, children }: Props) => {
-    useEffect(() => {
-        if (isOpen) {
-            const scrollTop = window.pageYOffset;
-            document.body.style.overflowY = "hidden";
-            window.scrollTo(0, scrollTop);
-        }
-        return () => {
-            const scrollTop = window.pageYOffset;
-            document.body.style.overflowY = "auto";
-            window.scrollTo(0, scrollTop);
-        }
-    }, [isOpen]);
-    return (
-        <>
-            {isOpen && <div className={styles["modal__wrapper"]}>
-                <div className={styles["modal__overlay"]} onClick={() => setOpen(false)}></div>
-                <div className={styles["modal__content"]}>
-                    <span className={styles["modal__closeButton"]} onClick={() => setOpen(false)}>X</span>
-                    {children}
-                </div>
-            </div>
-            }
-        </>
-    )
-}
+  const container = usePortal();
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflowY = "hidden";
+    }
+    return () => {
+      document.body.style.overflowY = "auto";
+    };
+  }, [isOpen]);
+  return createPortal(
+    <>
+      {isOpen && (
+        <div className={styles["modal__wrapper"]}>
+          <div
+            className={styles["modal__overlay"]}
+            onClick={() => setOpen(false)}
+          ></div>
+          <div className={styles["modal__content"]}>
+            <span
+              className={styles["modal__closeButton"]}
+              onClick={() => setOpen(false)}
+            >
+              X
+            </span>
+            {children}
+          </div>
+        </div>
+      )}
+    </>,
+    container,
+  );
+};
 
 Modal.defaultProps = {
-    isOpen: false,
-    setOpen: () => null
-}
+  isOpen: false,
+  setOpen: () => null,
+};
 
-export default Modal
+export default Modal;
