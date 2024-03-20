f455891f44b4c3263571399d249d27811f11c431
chadjaa-sofiane <sofianne012@gmail.com>
2024-03-20 22:34:36 +0100

    Change the style of the InputField component.

--- components/core/InputField/InputField.tsx
+++ components/core/InputField/InputField.tsx
@@ -1,30 +1,43 @@
+import { useId } from "react";
 import styles from "./InputField.module.scss";
 
 interface props
   // eslint-disable-next-line no-undef
   extends React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
-  label: string;
+  label?: string;
+  placeholder?: string;
   type?: "text" | "email" | "textarea";
   name: string;
   error?: string;
 }
 
 const InputField = ({
-  label,
   name,
   type = "text",
   error = "",
+  label = "",
+  placeholder = "",
+  style,
   ...rest
 }: props) => {
+  const id = useId();
   return (
-    <div className={styles["input__field"]}>
-      <label htmlFor="name">{label}</label>
-
-      {type === "textarea" ? (
-        <textarea {...rest} name={name} />
-      ) : (
-        <input {...rest} type={type} name={name} />
-      )}
+    <div style={style} className={styles["input__field"]}>
+      <label htmlFor={id} className={styles["input__field__container"]}>
+        {type === "textarea" ? (
+          <textarea {...rest} id={id} name={name} placeholder={placeholder} />
+        ) : (
+          <input
+            {...rest}
+            id={id}
+            type={type}
+            name={name}
+            placeholder={placeholder}
+          />
+        )}
+        <label className={styles["input__field__label"]}>{label}</label>
+        <div className={styles["input__field__underline"]}></div>
+      </label>
       <p className={styles["input__field__error"]}>{error}</p>
     </div>
   );
