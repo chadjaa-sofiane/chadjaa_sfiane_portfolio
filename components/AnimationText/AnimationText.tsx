f6b9caf2294adfe70c84be1cda67223099e129da
chadjaa-sofiane <sofianne012@gmail.com>
2024-03-21 22:30:01 +0100

    Update the AnimationText component to accept different titles.

--- components/AnimationText/AnimationText.tsx
+++ components/AnimationText/AnimationText.tsx
@@ -1,41 +1,59 @@
 import { useState, useRef, useEffect } from "react";
-import { Title1 } from "../core/Typography/Typography";
-import { gsap } from "gsap"
-import { v4 as uuidV4 } from "uuid"
+import { Title1, Title2, Title3, Title4 } from "../core/Typography/Typography";
+import { gsap } from "gsap";
+import { v4 as uuidV4 } from "uuid";
 
 interface Props {
-    children: string;
+  children: string;
+  tag?: "Title1" | "Title2" | "Title3" | "Title4";
 }
 
-const AnimationText = ({ children }: Props) => {
-    const ref = useRef(null)
-    const [dataLetter, setDataLetter] = useState<string | null>(null);
-    useEffect(() => {
-        const { current } = ref;
-        if (!dataLetter) setDataLetter(`letter-${uuidV4()}`);
-        if (current && dataLetter) {
-            const letters = document.querySelectorAll(`[data-letter= "${dataLetter}"]`);
-            // animate letters with gsap 
-            gsap.set(letters, { autoAlpha: 0, position: "relative", left: 10, top: -40 });
-            gsap.to(letters, {
-                scrollTrigger: current,
-                duration: 0.35,
-                autoAlpha: 1,
-                top: 0,
-                left: 0,
-                stagger: 0.1,
-                ease: "power2.out"
-            });
-        }
-    }, [dataLetter])
-    return (<Title1 ref={ref}>
-        {children.split("").map((c, i) => (
-            <span data-letter={dataLetter} key={i} >
-                {c}
-            </span>
-        ))}
-    </Title1>)
-}
+const TitleComponent = {
+  Title1: Title1,
+  Title2: Title2,
+  Title3: Title3,
+  Title4: Title4,
+};
 
+const AnimationText = ({ children, tag = "Title1" }: Props) => {
+  const ref = useRef(null);
+  const [dataLetter, setDataLetter] = useState<string | null>(null);
+  const Title = TitleComponent[tag];
+  useEffect(() => {
+    const { current } = ref;
+    if (!dataLetter) setDataLetter(`letter-${uuidV4()}`);
+    if (current && dataLetter) {
+      const letters = document.querySelectorAll(
+        `[data-letter= "${dataLetter}"]`,
+      );
+      // animate letters with gsap
+      gsap.set(letters, {
+        autoAlpha: 0,
+        position: "relative",
+        left: 10,
+        top: -40,
+      });
+      gsap.to(letters, {
+        scrollTrigger: current,
+        duration: 0.35,
+        autoAlpha: 1,
+        top: 0,
+        left: 0,
+        stagger: 0.1,
+        ease: "power2.out",
+      });
+    }
+  }, [dataLetter]);
+  return (
+    <Title ref={ref}>
+      {children.split("").map((c, i) => (
+        <span data-letter={dataLetter} key={i}>
+          {c}
+        </span>
+      ))}
+    </Title>
+  );
+};
 
-export default AnimationText
+export default AnimationText;
+
