721b62999c03ea52882edcd55df2e38b3edd8892
chadjaa-sofiane <sofiane.chadjaa@3s-pay.com>
2025-12-16 17:09:58 +0100

    Feat: update projectField component to include ProjectLegend.

--- containers/Projects/ProjectsField/ProjectsField.tsx
+++ containers/Projects/ProjectsField/ProjectsField.tsx
@@ -1,7 +1,5 @@
-import { useRouter } from "next/router";
 import { Title2 } from "@components/core/Typography";
 import { Section } from "@components/Section";
-import { Tabs, Tab } from "@components/core/Tabs";
 import ProjectsCards from "./ProjectsCards";
 import { cardProps } from "@components/Card";
 import styles from "./ProjectsField.module.scss";
@@ -9,25 +7,12 @@ import { useRef } from "react";
 
 
 const ProjectsField = ({ projects }: { projects: cardProps[] }) => {
-  const { push, query } = useRouter();
-
-  const defaultValue = (query.type as string) || "website";
-
   const divRef = useRef(null);
-  const handleAction = (name: string) => push({
-    pathname: "/projects",
-    query: { type: name },
-  }, undefined, { scroll: false });
 
   return (
     <Section>
       <div className={styles["projects__field"]} ref={divRef}>
         <Title2>  My Projects  </Title2>
-        <Tabs handleAction={handleAction} defaultValue={defaultValue} className={styles["projects__tabs"]}>
-          <Tab name="website"> Websites </Tab>
-          <Tab name="experiment"> Experiments </Tab>
-          <Tab name="ML"> Machine Learning </Tab>
-        </Tabs>
         <ProjectsCards projects={projects} />
       </div>
     </Section>
