ebb06fabc0fb305d750d3c60be030a7a4e87583d
chadjaa-sofiane <sofiane.chadjaa@3s-pay.com>
2026-01-05 16:08:29 +0100

    Style: update the header style.

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -1,45 +1,28 @@
-/* eslint-disable no-unused-vars */
-import { useState } from "react"
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Tabs, Tab } from "@components/core/Tabs";
 import styles from "./Header.module.scss";
 
 const NavBar = () => {
   const { pathname } = useRouter();
-  const [open, setOpen] = useState(false);
-  const closeMenu = () => setOpen(false);
+
+  const isActive = (path: string) => pathname === path ? styles["navLink--active"] : "";
+
   return (
     <nav className={styles["navBar"]}>
-      <Tabs handleAction={closeMenu} className={`${styles["navBar__list"]} ${open ? styles["navBar__list--open"] : ""}`} defaultValue={pathname}>
-        <Tab name="/">
-          <Link href="/">home</Link>
-        </Tab>
-        <Tab name="/projects">
-          <Link href="/projects">projects</Link>
-        </Tab>
-      </Tabs>
-      {/* <MenuIcon open={open} setOpen={setOpen} /> */}
+      <ul className={styles["navBar__list"]}>
+        <li>
+          <Link href="/" className={`${styles["navLink"]} ${isActive("/")}`}>
+            home
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects" className={`${styles["navLink"]} ${isActive("/projects")}`}>
+            projects
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
-
-// type menuIconProps = {
-//   open: boolean,
-//   setOpen: (open: boolean) => void
-// }
-
-// const MenuIcon = ({ open, setOpen }: menuIconProps) => {
-//   return (
-//     <>
-//       <input onChange={(e) => setOpen(e.target.checked)} checked={open} type="checkbox" className={styles["menu__input"]} hidden id="menu" />
-//       <label htmlFor="menu" className={styles["menu__icon"]}>
-//         <span></span>
-//         <span></span>
-//         <span></span>
-//       </label>
-//     </>
-//   )
-// }
 
 export default NavBar;
