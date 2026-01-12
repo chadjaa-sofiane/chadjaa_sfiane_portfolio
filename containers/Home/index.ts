4803ce8e63b0b80ec1e8d31307eab4898daa7b14
chadjaa-sofiane <sofiane.chadjaa@3s-pay.com>
2026-01-12 18:15:35 +0100

    Chore: add experienceSection to export.

--- containers/Home/index.ts
+++ containers/Home/index.ts
@@ -2,3 +2,4 @@ export { default as AboutMe } from "./AboutMe";
 export { default as DynamicRoleSection } from "./DynamicRoleSection";
 export * from "./HeroSection";
 export * from "./sections";
+export * from "./ExperienceSection";
