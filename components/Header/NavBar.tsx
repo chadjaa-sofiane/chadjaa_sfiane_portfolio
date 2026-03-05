6ad5a24c67791c415724fdf0942e5f166a3c30b2
chadjaa-sofiane <sofiane.chadjaa@3s-pay.com>
2026-03-05 22:42:49 +0100

    feat(theme-2): finalize distinctive theme and mobile nav

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -1,22 +1,43 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import styles from "./Header.module.scss";
 
 const NavBar = () => {
   const { pathname } = useRouter();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const isActive = (path: string) => pathname === path ? styles["navLink--active"] : "";
 
   return (
-    <nav className={styles["navBar"]}>
+    <nav className={`${styles["navBar"]} ${menuOpen ? styles["navBar--open"] : ""}`}>
+      <button
+        type="button"
+        className={styles["menuButton"]}
+        aria-label="Toggle navigation"
+        aria-expanded={menuOpen}
+        onClick={() => setMenuOpen((prev) => !prev)}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
       <ul className={styles["navBar__list"]}>
         <li>
-          <Link href="/" className={`${styles["navLink"]} ${isActive("/")}`}>
+          <Link
+            href="/"
+            className={`${styles["navLink"]} ${isActive("/")}`}
+            onClick={() => setMenuOpen(false)}
+          >
             home
           </Link>
         </li>
         <li>
-          <Link href="/projects" className={`${styles["navLink"]} ${isActive("/projects")}`}>
+          <Link
+            href="/projects"
+            className={`${styles["navLink"]} ${isActive("/projects")}`}
+            onClick={() => setMenuOpen(false)}
+          >
             projects
           </Link>
         </li>
