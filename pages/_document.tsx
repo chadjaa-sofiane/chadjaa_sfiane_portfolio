e59f5ef05f308e725b9afebe055a130685557eed
chadjaa-sofiane <sofiane.chadjaa@3s-pay.com>
2026-03-05 22:29:13 +0100

    feat(theme-1): redesign site visual language and unify component styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Oldenburg&family=Open+Sans:wght@300;400;700&family=Pavanam&family=Petit+Formal+Script&family=Poppins:wght@300;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400;9..144,600;9..144,700&family=Manrope:wght@400;500;700;800&family=Syne:wght@600;700&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" sizes="196x196" />
